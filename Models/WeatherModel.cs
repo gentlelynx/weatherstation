e963fa8600bbbc85111aec34a5cce2dc4170347c
gentlelynx <anastasiya.kjartansdottir@gmail.com>
2025-10-21 20:43:53 +0200

    attempting to fetch some data inside the Location component (CreatedTime)

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -2,28 +2,43 @@ using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace WeatherStation
 {
-    public class WeatherObject
+    public class WeatherClass
     {
-        public class Geometry
+        //var type VariableName
+        public string? CreatedTime { get; set; }
+        //var type of class[] variable 
+        public override string ToString()
         {
-            public string[]? Coordinates { get; set; }
+            return CreatedTime;
         }
 
-        public class TimeSeries
-        {
-            public Data[]? Data { get; set; }
-        }
+//         public Geometries[]? Geometry { get; set; }
+//         public TimeSerie[]? TimeSeries { get; set; }
+  
+//         public class Geometries
+//         {
+//             public string? Coordinates { get; set; }
+//             public override string ToString()
+//             {
+//                 return Coordinates;
+//             }
+//         }
 
-        public class Data
-        {
-            public int Air_temperature { get; set; }
-            public int Wind_speed { get; set; }
-            public int Wind_from_direction { get; set; }
-            public int Wind_speed_of_gust { get; set; }
-            public int Relative_humidity { get; set; }
-            public int Air_pressure_at_mean_sea_level { get; set; }
-            public int Precipitation_amount_min { get; set; }
-            public int Precipitation_amount_max { get; set; }
-        }
-    }
-}
+//         public class TimeSerie
+//         {
+//             public Data[]? Data { get; set; }
+//         }
+
+//         public class Data
+//         {
+//             public string? Air_temperature { get; set; }
+//             public int Wind_speed { get; set; }
+//             public int Wind_from_direction { get; set; }
+//             public int Wind_speed_of_gust { get; set; }
+//             public int Relative_humidity { get; set; }
+//             public int Air_pressure_at_mean_sea_level { get; set; }
+//             public int Precipitation_amount_min { get; set; }
+//             public int Precipitation_amount_max { get; set; }
+//         }
+     }
+ }
