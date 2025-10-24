6f9ddc12fc58023b0f3f0775d308c40b7d214758
gentlelynx <anastasiya.kjartansdottir@gmail.com>
2025-10-24 21:03:16 +0200

    Added fonts, layout and temporary css tweaks

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -5,10 +5,9 @@ namespace WeatherStation
     public class WeatherClass
     {
         //var type VariableName
-        public string? CreatedTime { get; set; }
-        public WeatherGeometry? Geometry { get; set; }
-        //var type of class[] variable 
-        public TimeSeriesEntry[]? TimeSeries { get; set; }
+        public string CreatedTime { get; set; } = "";
+        public WeatherGeometry? Geometry { get; set; } 
+        public TimeSeriesEntry[] TimeSeries { get; set; } = [];
         public override string ToString()
         {
             return TimeSeries[1].Data.Air_temperature.ToString();
@@ -16,24 +15,24 @@ namespace WeatherStation
     }
     public class WeatherGeometry
     {
-        public string? Type { get; set; }
-        public float[]? Coordinates { get; set; }
+        public string Type { get; set; } = "";
+        public float[] Coordinates { get; set; } = [];
 
     }
     public class TimeSeriesEntry
     {
-        public string? Time { get; set; }
-        public DataEntry Data { get; set; }
+        public string Time { get; set; } = "";
+        public DataEntry? Data { get; set; }
         public class DataEntry
         {
-            public float? Air_temperature { get; set; }
-            public float? Wind_speed { get; set; }
-            public int? Wind_from_direction { get; set; }
-            public float? Wind_speed_of_gust { get; set; }
-            public int? Relative_humidity { get; set; }
-            public float? Air_pressure_at_mean_sea_level { get; set; }
-            public float? Precipitation_amount_min { get; set; }
-            public float? Precipitation_amount_max { get; set; }
+            public float Air_temperature { get; set; } = 0;
+            public float Wind_speed { get; set; } = 0;
+            public int Wind_from_direction { get; set; } = 0;
+            public float Wind_speed_of_gust { get; set; } = 0;
+            public int Relative_humidity { get; set; } = 0;
+            public float Air_pressure_at_mean_sea_level { get; set; } = 0;
+            public float Precipitation_amount_min { get; set; } = 0;
+            public float Precipitation_amount_max { get; set; } = 0;
         }
     }
 }
